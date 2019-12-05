9845bd0fa0ceb09c9b4bd2e798d35531010615af
John Reiland <reiland.shanghai@gmail.com>
2019-12-05 18:36:13 -0500

    "Update eslint config"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-  extends: 'airbnb-base'
+  extends: 'airbnb-base',
+  rules: {
+    "import/no-extraneous-dependencies": ["error", {"devDependencies": true}],
+    "arrow-parens": ["error", "always"],
+    "no-console": ["error", {allow: ["log", "warn", "error"]}],
+    "no-plusplus": "off",
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
 };
