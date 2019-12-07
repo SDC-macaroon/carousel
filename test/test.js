e2271185a05e070101b2aa8ed664e16cf05e7f00
John Reiland <reiland.shanghai@gmail.com>
2019-12-06 23:14:05 -0500

    "Add query functionality tests"

--- test/test.js
+++ test/test.js
@@ -3,13 +3,13 @@ const react = require('react');
 const enzyme = require('enzyme');
 const puppeteer = require('puppeteer');
 
-const pageUrl = 'http//localhost:3001/morestyles/2001';
+const pageUrl = 'http://localhost:3001/morestyles/';
 let page;
 let browser;
-const width = 1920;
-const height = 1080;
+const width = 1080;
+const height = 720;
 
-const beforeAll = (async () => {
+beforeAll(async () => {
   browser = await puppeteer.launch({
     headless: false,
     slowMo: 80,
@@ -19,12 +19,21 @@ const beforeAll = (async () => {
   await page.setViewport({ width, height });
 });
 
-const afterAll = (() => {
+afterAll(() => {
   browser.close();
 });
 
-describe('test whether jest installed correctly', () => {
-  test('jest "test" with no test defined passes by default', () => {
+describe('/morestyles/ queries return correct responses', () => {
+  test('query of invalid productId returns error', async () => {
+    await page.goto(`${pageUrl}190`);
+    const response = await page.$eval('pre', (e) => e.textContent);
+    expect(response).toBe('a problem occured with the request');
+  });
 
+  test('query of productId 2001 returns array of styles begining with productId 2002', async () => {
+    await page.goto(`${pageUrl}2001`);
+    const response = await page.$eval('pre', (e) => e.textContent);
+    expect(Array.isArray(JSON.parse(response))).toBe(true);
+    expect(JSON.parse(response)[0].productId).toBe(2002);
   });
 });
